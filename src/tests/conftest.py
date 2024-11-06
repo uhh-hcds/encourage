c42c7a81a6f9fb4ce6f3491add5d3a1c9f495b2e
strich <jan.strich@uni-hamburg.de>
2024-11-06 12:57:25 +0100

    fix(*): add nltk to project for fixing tests

---
+++ src/tests/conftest.py
@@ -0,0 +1,12 @@
+"""Pytest configuration file."""
+
+import nltk
+
+
+def pytest_configure(config):
+    """Allows plugins and conftest files to perform initial configuration.
+
+    This hook is called for every plugin and initial conftest
+    file after command line options have been parsed.
+    """
+    nltk.download("punkt")
