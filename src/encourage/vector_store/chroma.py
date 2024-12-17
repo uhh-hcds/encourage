a493747d8ae78765cdf1d00065c8a8c3d1ca8bfa
strich <jan.strich@uni-hamburg.de>
2024-12-17 18:48:52 +0100

    feat(*): add chroma to repo

--- src/encourage/vector_store/chroma.py
+++ src/encourage/vector_store/chroma.py
@@ -1,13 +1,62 @@
 """Chroma vector store implementation."""
 
 import logging
+from contextlib import suppress
+from typing import Any
 
-from encourage.vector_store.vector_store import VectorStore
+import chromadb
+from llama_index.core.vector_stores.types import BasePydanticVectorStore
+from llama_index.vector_stores.chroma import ChromaVectorStore
+
+from encourage.vector_store.vector_store import VectorStore, VectorStoreBatch
 
 logger = logging.getLogger(__name__)
 
 
-class ChromaCustomClient(VectorStore):
+class ChromaClient(VectorStore):
     """Chroma vector store implementation."""
 
-    pass
+    def __init__(self) -> None:
+        self.client = chromadb.Client()
+
+    def create_collection(
+        self,
+        collection_name: str,
+        distance: str = "cosine",
+        size: int = 1000,
+        overwrite: bool = False,
+    ) -> None:
+        """Create a collection."""
+        if overwrite:
+            with suppress(ValueError):
+                self.client.delete_collection(collection_name)
+        self.client.create_collection(
+            name=collection_name, metadata={"distance": distance, "size": size}
+        )
+
+    def delete_collection(self, collection_name: str) -> None:
+        """Delete a collection."""
+        self.client.delete_collection(name=collection_name)
+
+    def insert_documents(
+        self, collection_name: str, vector_store_document: VectorStoreBatch
+    ) -> None:
+        """Insert documents."""
+        collection = self.client.get_collection(name=collection_name)
+
+        documents = [document.content for document in vector_store_document.documents]
+        meta_data = [document.meta_data for document in vector_store_document.documents]
+        ids = [str(document.id) for document in vector_store_document.documents]
+
+        collection.add(documents=documents, metadatas=meta_data, ids=ids)  # type: ignore
+
+    def query(self, collection_name: str, query: list, top_k: int, **kwargs: Any) -> dict:
+        """Query the collection."""
+        collection = self.client.get_collection(name=collection_name)
+        result = collection.query(query_texts=query, n_results=top_k, **kwargs)
+        return dict(result)
+
+    def get_llama_index_class(self, collection_name: str) -> BasePydanticVectorStore:
+        """Get the Llama index class."""
+        collection = self.client.get_collection(name=collection_name)
+        return ChromaVectorStore(chroma_collection=collection)
