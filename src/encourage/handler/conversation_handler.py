1bc123bbb066747f0563fe51579e9307ca2ccae5
strich <jan.strich@uni-hamburg.de>
2025-01-16 17:14:04 +0100

    refactor(conversation_handler): update template name and improve user prompt handling

--- src/encourage/handler/conversation_handler.py
+++ src/encourage/handler/conversation_handler.py
@@ -7,6 +7,7 @@ from encourage.llm.response_wrapper import ResponseWrapper
 from encourage.prompts.context import Context
 from encourage.prompts.conversation import Conversation
 from encourage.prompts.meta_data import MetaData
+from encourage.prompts.prompt import Prompt
 from encourage.prompts.prompt_reformatter import PromptReformatter
 
 
@@ -19,7 +20,7 @@ class ConversationHandler:
     user_prompts: list[str]
     contexts: list[Context] = field(default_factory=list)
     meta_data: list[MetaData] = field(default_factory=list)
-    template_name: str = "generate_python_code_conv.j2"
+    template_name: str = "llama3_conv.j2"
     conversation: Conversation = field(init=False)
 
     def run(
@@ -41,18 +42,26 @@ class ConversationHandler:
         request_outputs = []
         for idx, user_prompt in enumerate(self.user_prompts):
             context = self.contexts[idx] if self.contexts else Context()
-            user_prompt_reformated = PromptReformatter.reformat_conversation(
-                user_prompt, context, template_name=self.template_name
+            conversation.add_message("user", user_prompt)
+            prompt = Prompt(
+                conversation=conversation,
+                context=context,
+                meta_data=self.meta_data[idx] if self.meta_data else MetaData(),
             )
-            conversation.add_message("user", user_prompt_reformated)
+            prompt = PromptReformatter.reformat_user_prompt(
+                prompt, template_name=self.template_name
+            )
 
-            request_output = self.chat_inference_runner.run(conversation)
+            request_output = self.chat_inference_runner.run(prompt, raw_output=True)
             request_outputs.append(request_output)
-            conversation.add_message("assistant", request_output.outputs[0].text or "")
+            conversation.add_message("assistant", request_output.choices[0].message.content or "")  # type: ignore
 
         self.conversation = conversation
         return ResponseWrapper.from_conversation(
-            request_outputs, conversation, self.contexts, self.meta_data
+            request_outputs,  # type: ignore
+            conversation,
+            self.contexts,
+            self.meta_data,
         )
 
     def __len__(self) -> int:
